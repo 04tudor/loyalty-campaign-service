42d1bddcfdc0a86361d1acafa1397414fcc3ecb0
Coretchi Tudor <tudorcoretchi119@gmail.com>
2024-04-25 13:14:03 +0300

    Final Commit

--- application/src/main/java/md/maib/retail/application/CampaignSomeInfo.java
+++ application/src/main/java/md/maib/retail/application/CampaignSomeInfo.java
@@ -17,11 +17,7 @@ public class CampaignSomeInfo {
 
     private final LoyaltyEventType loyaltyEventType;
 
-    public static CampaignSomeInfo valueOf(CampaignId id, CampaignMetaInfo metaInfo,
-                                           Interval activityInterval, CampaignState state,
-                                           LoyaltyEventType loyaltyEventType) {
-        return new CampaignSomeInfo(id, metaInfo, activityInterval, state, loyaltyEventType);
-    }
+
     public static CampaignSomeInfo valueOf(Campaign campaign) {
         return new CampaignSomeInfo(campaign.getId(), campaign.getMetaInfo(),
                 campaign.getActivityInterval(), campaign.getState(),
