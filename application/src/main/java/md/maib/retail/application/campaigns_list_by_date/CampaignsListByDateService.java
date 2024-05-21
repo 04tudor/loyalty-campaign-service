19be41e73fde4e01d2f5f2514a059a89b70e6ab3
Coretchi Tudor <tudorcoretchi119@gmail.com>
2024-05-21 17:01:39 +0300

    Edited test for list by date

--- application/src/main/java/md/maib/retail/application/campaigns_list_by_date/CampaignsListByDateService.java
+++ application/src/main/java/md/maib/retail/application/campaigns_list_by_date/CampaignsListByDateService.java
@@ -1,14 +1,11 @@
 package md.maib.retail.application.campaigns_list_by_date;
 
 import md.maib.retail.application.CampaignSomeInfo;
-import md.maib.retail.model.campaign.CampaignState;
 import md.maib.retail.model.ports.Campaigns;
-import org.threeten.extra.Interval;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class CampaignsListByDateService implements CampaignsListByDateUseCase{
     private final Campaigns campaigns;
@@ -21,15 +18,9 @@ public class CampaignsListByDateService implements CampaignsListByDateUseCase{
     public List<CampaignSomeInfo> activeCampaignsByDate(LocalDate date) {
         return campaigns.listByDate(date)
                 .stream()
-                .filter(campaign -> isDateWithinInterval(CampaignSomeInfo.valueOf(campaign), date) && campaign.getState() == CampaignState.ACTIVE)
                 .map(CampaignSomeInfo::valueOf)
                 .toList();
     }
 
-    private boolean isDateWithinInterval(CampaignSomeInfo campaign, LocalDate date) {
-        Interval interval = campaign.interval();
-        LocalDate start = interval.getStart().atZone(java.time.ZoneId.systemDefault()).toLocalDate();
-        LocalDate end = interval.getEnd().atZone(java.time.ZoneId.systemDefault()).toLocalDate();
-        return !date.isBefore(start) && !date.isAfter(end);
-    }
+//
 }
