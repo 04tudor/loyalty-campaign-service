dcebc04e846f50f2cb98b724950060a1d3e6f8fd
Coretchi Tudor <tudorcoretchi119@gmail.com>
2024-05-28 13:50:06 +0300

    Tests for find by id and delete campaign success

--- infrastructure/adapters/persistence/src/test/java/md/maib/retail/infrastructure/persistence/test/SpringDataJpaCampaignRepositoryAdapterTest.java
+++ infrastructure/adapters/persistence/src/test/java/md/maib/retail/infrastructure/persistence/test/SpringDataJpaCampaignRepositoryAdapterTest.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import md.maib.retail.infrastructure.persistence.CampaignRecord;
 import md.maib.retail.infrastructure.persistence.RuleRecord;
 import md.maib.retail.infrastructure.persistence.SpringDataJpaProjectRepositoryAdapter;
+import md.maib.retail.model.campaign.Campaign;
 import md.maib.retail.model.campaign.CampaignId;
 import md.maib.retail.model.campaign.FieldType;
 import md.maib.retail.model.conditions.Condition;
@@ -16,10 +17,14 @@ import md.maib.retail.model.effects.LoyaltyEffectType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @PersistenceTest
  class SpringDataJpaCampaignRepositoryAdapterTest {
@@ -49,15 +54,27 @@ import java.util.*;
 
     }
 
-    @DataSet(value = "datasets/campaigns.yaml")
+    @Sql("/datasets/campaigns.sql")
     @Test
-    void findById(){
-        CampaignId campaignId= CampaignId.valueOf(UUID.fromString("1e7e7d50-9f9f-4b7c-bd9b-5f5f3d0f7f7f"));
-        var campaign=repository.findById(campaignId);
+    void findById() {
+        CampaignId campaignId = CampaignId.valueOf(UUID.fromString("1e7e7d50-9f9f-4b7c-bd9b-5f5f3d0f7f7f"));
+        Optional<Campaign> optionalCampaign = repository.findById(campaignId);
 
-        Assertions.assertNotNull(campaign);
-        Assertions.assertEquals(campaignId, campaign.get().getId());
+        assertTrue(optionalCampaign.isPresent(), "Campaign not found");
 
+        Campaign campaign = optionalCampaign.get();
+        Assertions.assertEquals(campaignId, campaign.getId());
     }
 
+    @Test
+    @Sql("/datasets/campaigns.sql")
+    void deleteCampaignRecord() {
+        CampaignId campaignId = CampaignId.valueOf(UUID.fromString("1e7e7d50-9f9f-4b7c-bd9b-5f5f3d0f7f7f"));
+
+        boolean deleted = repository.delete(campaignId);
+
+
+        assertTrue(deleted, "Campaign record was not deleted");
+
+    }
 }
---
+++ infrastructure/adapters/persistence/src/test/resources/datasets/campaigns.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO campaigns.campaign (campaign_id, metainfo, interval_start, interval_end, is_active, loyalty_event_type_id)
+VALUES ('1e7e7d50-9f9f-4b7c-bd9b-5f5f3d0f7f7f', '{"key": "value"}', '2024-06-01T00:00:00Z'::TIMESTAMPTZ, '2024-06-30T23:59:59Z'::TIMESTAMPTZ, true, '123e4567-e89b-12d3-a456-426614174000');
